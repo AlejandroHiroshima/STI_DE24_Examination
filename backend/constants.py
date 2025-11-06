fc82d1d729a0824b61770504de249da0c3b65e2e
Alexander Hrachovina <alexander.hrachovina@stud.sti.se>
2025-11-06 15:53:13 +0100

    dlt for strength historical data progress

--- backend/constants.py
+++ backend/constants.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 
-CSV_URL = "https://docs.google.com/spreadsheets/d/1gW5Yjn3FqWePsQzxXogLH2Qvpv83bzPmZmpyzBvzrhs/export?format=csv&gid=0"
-DUCKDB_PATH = Path(__file__).parent / "data"
+CSV_URL = "https://docs.google.com/spreadsheets/d/1gW5Yjn3FqWePsQzxXogLH2Qvpv83bzPmZmpyzBvzrhs/gviz/tq?tqx=out:csv&sheet=Erik"
+DUCKDB_PATH = Path(__file__).parents[1] / "data" / "strength.duckdb"
 
+print(DUCKDB_PATH)
