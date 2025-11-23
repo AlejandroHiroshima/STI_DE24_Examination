1fc9c0c23a6aa55fbb06e1461c1fffb86ac6ab48
Erik Unevik <erikunevik@gmail.com>
2025-11-23 16:12:23 +0100

    dim

---
+++ backend/dbt_code/models/dim/dim_exercises.sql
@@ -0,0 +1,10 @@
+with dim_exercise as (select * from {{ref 'src_exercise' }})
+
+select
+    {{ dbt_utils.generate_surrogate_key(['exercise_name', 'muscle_group_primary'])}} as exercise_id,
+    exercise_name,
+    muscle_group_primary,
+    muscle_group_secondary
+from
+    dim_exercise
+
---
+++ backend/dbt_code/models/fct/fct_strength.sql
@@ -0,0 +1,1 @@
+with fct_strength 
