7d2dff45607274393349bd33f3f569f43abea748
Alexander Hrachovina <alexander.hrachovina@stud.sti.se>
2025-11-27 15:49:52 +0100

    more taipy'ing

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -8,22 +8,23 @@ with tgb.Page() as start_page:
 
     with tgb.part():
         with tgb.part(class_name="text-center card-margin"):
-            with tgb.layout(columns=("1 1")):
-                with tgb.part():
-                    tgb.text('### **Erik**', mode="md")
-                    tgb.image(
-                        "<lägg image här>", # image
-                        on_action= <länk till athletssida> #
-                        class_name = "hover_color picture_style",
-                        height= "150px",
-                        width= "150px"      
-                    )
+            with tgb.part(class_name="card card-margin"):
+                with tgb.layout(columns=("1 1")):
+                    with tgb.part():
+                        tgb.text('### **Erik**', mode="md")
+                        tgb.image(
+                            "<lägg image här>", # image
+                            on_action= <länk till athletssida> #
+                            class_name = "hover_color picture_style",
+                            height= "150px",
+                            width= "150px"      
+                        )
 
-                    tgb.text('### **Alex**', mode="md")
-                    tgb.image(
-                        "<lägg image här>", # image
-                        on_action= <länk till athletssida> #
-                        class_name = "hover_color picture_style",
-                        height= "150px",
-                        width= "150px"      
-                    )
+                        tgb.text('### **Alex**', mode="md")
+                        tgb.image(
+                            "<lägg image här>", # image
+                            on_action= <länk till athletssida> #
+                            class_name = "hover_color picture_style",
+                            height= "150px",
+                            width= "150px"      
+                        )
