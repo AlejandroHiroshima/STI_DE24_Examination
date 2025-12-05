3b5d5e0f39a79ede5c82e9bd055fdece4e1ecf92
Alexander Hrachovina <alexander.hrachovina@stud.sti.se>
2025-12-05 18:46:18 +0100

    Seed CSV

--- backend/dbt_code/models/dim/dim_exercises.sql
+++ backend/dbt_code/models/dim/dim_exercises.sql
@@ -1,13 +1,36 @@
-with dim_exercise as (
-    select distinct
-        exercise_name,
-        muscle_group_primary,
-    from {{ref ('src') }})
+with src as (
+    SELECT DISTINCT(TRIM(exercise_name)) AS exercise_name_raw,
+        muscle_group_primary
+    FROM {{ ref('src') }}
+),
+
+cleaned as (
+    SELECT
+        REGEXP_REPLACE(
+            REGEXP_REPLACE(
+                LOWER(REPLACE(exercise_name_raw, '-', ' ')),
+                ' {2,}', ' ', 'g'
+            ),
+            ' $', '', 'g'
+        ) AS exercise_name,
+        muscle_group_primary
+
+    FROM src
+), normalized AS (
+    SELECT
+        CASE 
+            WHEN exercise_name IS 'bench press' THEN 'benchpress'
+            WHEN exercise_name IS 'chinup' THEN 'chin-up'
+            ELSE exercise_name
+        END AS exercise_name,
+        muscle_group_primary
+    FROM cleaned
+),
 
 select
     {{ dbt_utils.generate_surrogate_key(['exercise_name', 'muscle_group_primary'])}} as exercise_id,
     exercise_name,
     muscle_group_primary,
 from
-    dim_exercise
+    cleaned
 
