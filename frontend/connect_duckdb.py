02341f029ae97a474562bb207edc2e68418cbf7e
Alexander Hrachovina <alexander.hrachovina@stud.sti.se>
2025-12-19 09:32:16 +0100

    started on dashboard but discorvered bug with athlete name

--- frontend/connect_duckdb.py
+++ frontend/connect_duckdb.py
@@ -1,24 +1,49 @@
 import duckdb
 from pathlib import Path
+import pandas as pd
 
-# DUCKB_PATH = Path(__file__).parents[1] / "strength.duckdb"
+db_path = Path(__file__).parents[1] / "strength.duckdb"
 
-def connect_duckdb(sql_code: str, parameters=None):
-    with duckdb.connect(Path(__file__).parents[1] / "strength.duckdb") as conn:
-        
-        connection = conn.execute(sql_code, parameters)
-        
-        return connection.df()
+def query_strength_duckdb(athlete_name: str, start_date: str, end_date: str) -> pd.DataFrame :
+    with duckdb.connect(str(db_path), read_only=True) as conn:
+        query=(f"""
+            SELECT 
+                *
+            FROM
+                main_mart.mart_strength
+            WHERE
+                LOWER(athlete_first_name) = LOWER(?)
+            AND
+                full_workout_date BETWEEN ? AND ?
+            AND 
+                exercise_name IS NOT NULL;    
+""")    
+        result = conn.execute(query, [athlete_name, start_date, end_date] )    
+        return result.df()
     
+# if __name__ == "__main__":
+    # Test med riktiga värden
+    # df = query_strength_duckdb("Erik", "2025-01-01", "2025-10-25")
     
-df = connect_duckdb("desc;")
-print(df.head())
+    # print(f"Rows fetched: {len(df)}")
+    # print(f"\nColumns: {df.columns.tolist()}")
+    
+    # if len(df) > 0:
+    #     print(f"\nFirst 5 rows:")
+    #     print(df.head())
+    #     print(f"\nData types:")
+    #     print(df.dtypes)
+    # else:
+    #     print("\n⚠️ No data found! Check:")
+    #     print("  - Database path:", db_path)
+    #     print("  - Athlete name spelling")
+    #     print("  - Date range")
+    # print(f"Looking for DB at: {db_path}")
+    # print(f"DB exists: {db_path.exists()}")    
 
 if __name__ == "__main__":
-    connect_duckdb()
- 
-    
-
-        
-    
-
+    with duckdb.connect(str(db_path), read_only=True) as conn:
+     print(conn.execute("""
+                SELECT DISTINCT athlete_first_name 
+                FROM main_mart.mart_strength
+            """).df())
--- frontend/strength.py
+++ frontend/strength.py
@@ -1,9 +1,25 @@
 import taipy.gui.builder as tgb
 from taipy.gui import navigate
-from connect_duckdb import connect_duckdb
+from connect_duckdb import query_strength_duckdb
+import datetime
+import pandas as pd
 
-people = ["erik", "alex"]
-selected_athlete = ""
+people = ["Erik", "alexander"]
+selected_athlete = "Erik"
+start_date = datetime.date(2014,1,6)
+end_date = datetime.date.today()
+dates = [start_date, end_date]
+show_data = False
+strength_data = pd.DataFrame()
+
+def on_filter_click(state):
+    athlete = state.selected_athlete
+    start_date = state.dates[0].strftime("%Y-%m-%d")
+    end_date = state.dates[1].strftime("%Y-%m-%d")
+    df= query_strength_duckdb(athlete, start_date, end_date)
+    state.strength_data = df
+    state.show_data= True
+
 
 with tgb.Page() as strength_page:
     tgb.toggle(theme=True)
@@ -15,14 +31,55 @@ with tgb.Page() as strength_page:
             with tgb.part(class_name="card card-margin"):
                 with tgb.layout(columns="1 1"):
                     with tgb.part():
-                        tgb.text("### Strength", mode="md")
                         tgb.selector(
                             value="{selected_athlete}",
                             lov=people,
                             dropdown=True,
-                            label = "Choose athlete",
-                            
+                            label = "Choose athlete"
                         )
+                    with tgb.part():
+                        tgb.date_range("{dates}", with_time=False, format="yyyy-MM-dd")
+
+                with tgb.part():
+                    tgb.button(
+                        "Filter",
+                        on_action= on_filter_click
+                    )        
+            with tgb.part(render="{show_data}"):
+                with tgb.part(class_name="card card-margin"):
+                    tgb.text("## KPI's", mode="md")
+
+                    with tgb.layout(columns="1 1 1"):
+                        with tgb.part(class_name="card"):
+                            tgb.text("**Total gym sessions**", mode="md")
+                            tgb.text("{len(strength_data['full_workout_date'].unique())}", class_name="h2")
+                
+                        with tgb.part(class_name="card"):
+                            tgb.text("**Total volume (kg)**", mode="md")
+                            if selected_athlete == 'Erik':
+                                tgb.text("{strength_data['total_volume_session'].sum()}", class_name="h2")
+                            else:
+                                tgb.text("{strength_data['weight_kg].sum()}", class_name="h2")
+
+                        with tgb.part(class_name="card"):
+                            tgb.text("**Total amount of sets**", mode="md")
+                            tgb.text("{len(strength_data)}", class_name="h2")
+
+                    with tgb.layout(columns="1 1 1"):
+                        with tgb.part(class_name="card"):
+                            tgb.text("**Total time spent**", mode="md")
+                            tgb.text("{(strength_data['time_session'].sum())}", class_name="h2")
+                
+                        with tgb.part(class_name="card"):
+                            tgb.text("**Total number repetitions**", mode="md")
+                            tgb.text("{strength_data['reps'].sum()}", class_name="h2")
+                    
+                        with tgb.part(class_name="card"):
+                            tgb.text("**Heaviest set**", mode="md")
+                            tgb.text("{strength_data['weight_kg'].max()}", class_name="h2")
+                        
+
+
                 tgb.button(
                     "Tillbaka till dashboard",
                     on_action=lambda state: navigate(state, to="/")
